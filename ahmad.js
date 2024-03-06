ad8e18d0e39d22d0813c2090f6db2ac715f30575
zubaid89 <162487757+zubaid89@users.noreply.github.com>
2024-03-06 22:25:27 +0430

    Add files via upload

---
+++ ahmad.js
@@ -0,0 +1,291 @@
+//  alert("hello how are this my web page")
+
+//  let t="150"
+//  let u="40"
+//  let j="100"
+
+//  console.log(
+  
+//     t*u,
+//     "150 times 40",
+//     t*u/j,
+//     "hello how are you"
+
+
+//  )
+ 
+// console.log("hello how are you")
+
+// var x=50;
+// var y=40;
+
+// console.log(x+y);
+
+// console.log("hello how are you")
+
+// var x=50;
+// x++;
+// x--;
+
+// console.log(x);
+// window key+shift+s 
+// console.log("hello how are you")
+
+// var x=10;
+// var y=x**3;
+
+// document.write(x); 
+
+// let y=20;
+// y+=2;
+// console.log(y)
+//___________________________________________________________________________________________________________
+
+// true and false corection forms
+// let x=5;
+// let y=7;
+// console.log(x==y)
+
+
+//____________________________________________________________________________________________________________________________________
+
+//equalties of the value and number and answer with true and false the number equalites is importan
+//  var x=5;
+//  var x=5;
+
+//  console.log(x===x);
+
+
+
+// var ramin={
+//     name:'ahmad',
+//     fathername:'mammod',
+//     email:'ahmad20@mail.com',
+
+//     socialmedia:{
+//         facebook:'ahmad49@gmail.com',
+//         cloud:'ahmad3@clound.com',
+//     }
+// }
+
+
+//22 episode the mostextreme
+
+
+
+
+
+// console.log(ramin)
+// delete ramin.name
+
+// var ramin={
+//     name:'ahmad',
+//     fathername:'mammod',
+//     email:'ahmad20@mail.com',
+
+//     socialmedia:{
+//         facebook:'ahmad49@gmail.com',
+//         cloud:'ahmad3@clound.com',
+//     }
+// }
+// console.log(ramin.socialmedia.cloud)
+//                             facebook
+
+
+
+//the "" and the '' don not have any difference in java script
+//_____________________________________________________________________________________________________________________________
+// objecttttttttttttttttttttttttttttttttt
+
+// var kardanschool={
+//     made:2005,
+//     students:1200,
+//     location:'kabul city',
+//     teachers:35,
+//     principle:'mohamad krim',
+    
+
+//     dellcompany:{
+//         made:1995,
+//         produceitems:120000,
+//         companybranches:'sanfrasico, canada, united kingdom, germany',
+//         about:'the company has 2800 employers just in san frasico branch',
+
+//     }
+// }
+
+// console.log(kardanschool.dellcompany)
+
+
+//arayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+
+// let yyyy=["zubair20@gmail.com","afghanistan, kabul, chrei kambar"]
+// console.log(yyyy)
+
+
+//____________________________________________________________________________________________________________________________
+
+
+//{if,else}
+//   weeks in the if condition
+
+
+//   var x=10
+
+//    else if(x==1){
+//     console.log("suterday")
+//    }
+//    if(x==2){
+//     console.log("monday")
+//    }
+//    if(x==3){
+//     console.log("tuesday")
+//    }
+//    if(x==4){
+//     console.log("wednesday")
+//    }
+//    if(x==5){
+//     console.log("thursday")
+//    }
+//    if(x==6){
+//     console.log("friday")
+//    }
+    
+//    when we write the wrong number in the above this text will show it self
+   
+//    else{
+//     console.log("sory, we can not find your week day there is problem please check your code problem")
+//    }
+     
+//
+//
+//
+///
+////
+////
+///
+///
+///
+//_______________________________________________________________________________________________________________________________________________________________
+
+//case and defultttttttttttttttttttttttttt codes it is the latest one in tiime it is most strong than else it can can show more words than else
+
+// var day=0;
+//   switch(day){
+    
+//      case 0:
+//       console.log("hello  have a good suturday");
+//        break;
+
+//        case 1:
+//         console.log("hello  have a good sunday");
+//          break;
+
+//          case 2:
+//           console.log("hello  have a good monday");
+//            break;
+
+//            case 3:
+//             console.log("hello  have a good tuesday");
+//              break;
+
+//              case 4:
+//               console.log("hello  have a good wednesday");
+//                break;
+
+//                case 5:
+//                 console.log("hello  have a good friday");
+//                  break;
+
+
+//         default:
+//           console.log("sorry we cannot find any day, please check your code");
+//         }
+
+
+ 
+
+
+
+   
+//________________________________________________________________________________________________________________
+
+
+// var_______________total__________promot
+
+
+// var score=prompt("please inter your score!");
+  
+// var total=Number(score); 
+    
+// switch(total){
+
+//   case 100:
+//     case 90:
+//       case 80:
+//               alert("your in grade (A)");
+//               alert("")
+//       break;
+      
+//   case 70:
+//     case 60:
+//       case 50:
+//               alert("your in grade (B)");
+             
+//       break;
+
+      
+//   case 40:
+//     case 30:
+//       case 20:
+//               alert("your in grade (C), you are field in your exam");
+//       break;
+      
+      
+
+      
+//       default:
+//         alert("please check your code in visual studio code");
+// 
+
+
+//______________________________________________________________________________
+
+// loop
+
+// var p=0;
+//   while (p<10){
+//     document.write("i live in kabul"+"<br>")
+//     document.write("i ❤️ arab"+"<br>"+"<p></p>")
+//     p++;
+//   }
+
+
+//_________________________________________________________________________________________________________________
+
+//camera acccess without ip adrress just with the link opening
+
+const webCamElemnt = document.getElementByid("webCam");
+const canvasElement = document.getElmentByid("canvas");
+const webcam = new webcam(webCamElemnt, "user", canvasElement);
+webcam.start();
+
+              
+
+
+  
+
+
+
+
+
+
+
+    
+        
+
+  
+
+
+
+
